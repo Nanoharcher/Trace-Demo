3812d9e82e718e0842ba430138504604d7ec9900
PudgeMeat <2474451540@qq.com>
2018-10-09 13:12:43 +0800

    Updated

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -79,20 +79,16 @@ module.exports = {
       },
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
-        }
-      },
-      {
-        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          name: utils.assetsPath('../fonts/[name].[hash:7].[ext]')
-        }
+        loader: 'url-loader'
       }
+      // {
+      //   test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+      //   loader: 'url-loader',
+      //   options: {
+      //     limit: 10000,
+      //     name: utils.assetsPath('../fonts/[name].[hash:7].[ext]')
+      //   }
+      // }
     ]
   },
   node: {
