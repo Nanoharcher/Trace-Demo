43a6f23e45e1569a972823cb7b8a94025a8ab791
PudgeMeat <2474451540@qq.com>
2018-10-11 17:06:23 +0800

    Updated
    Change-Id: I9e2c99304d86d9fa0a0e1ec4ea400ca257a4d0f2

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,19 +1,14 @@
 <template>
   <div class="navigation-wrapper">
-    <el-menu :default-active="activeIndex" class="navigation-content" mode="horizontal"
+    <el-menu class="navigation-content" mode="horizontal"
              background-color="#545c64" text-color="#fff" active-text-color="#ffd04b" :router="true">
       <!--<el-menu-item index="1">Trace平台</el-menu-item>-->
       <el-submenu index="1">
         <template slot="title"><span class="navigation-title">机器学习</span></template>
-        <el-menu-item index><a href="http://sia.baidu.com/p/feed#/conf/result/dashboardgroup~name=_1760289&menuId=150206" target="_blank">大盘波动追查指标监控</a></el-menu-item>
-        <el-menu-item index="/mlSearch">Predictor日志检索</el-menu-item>
-        <el-menu-item index="/roughsort">粗排日志检索</el-menu-item>
-        <el-submenu index="1-4">
-          <template slot="title"><a href="http://ftrace.baidu.com/ml/index.html" target="_blank">机器学习导航</a></template>
-          <el-menu-item index="1-4-1">测试选项1</el-menu-item>
-          <el-menu-item index="1-4-2">测试选项2</el-menu-item>
-          <el-menu-item index="1-4-3">测试选项3</el-menu-item>
-        </el-submenu>
+        <a href="http://sia.baidu.com/p/feed#/conf/result/dashboardgroup~name=_1760289&menuId=150206" target="_blank"><el-menu-item index="1-1">大盘波动追查指标监控</el-menu-item></a>
+        <el-menu-item index="mlSearch">Predictor日志检索</el-menu-item>
+        <el-menu-item index="roughsort">粗排日志检索</el-menu-item>
+        <a href="http://ftrace.baidu.com/ml/index.html" target="_blank"><el-menu-item index="1-4">机器学习导航</el-menu-item></a>
       </el-submenu>
       <el-submenu index="2">
         <template slot="title"><span class="navigation-title">在线模块</span></template>
@@ -24,17 +19,17 @@
       </el-submenu>
       <el-submenu index="3">
         <template slot="title"><span class="navigation-title">用户模型</span></template>
-        <el-menu-item index="3-1"><a href="http://ftrace.baidu.com/ums/cuid.html" target="_blank">用户模型扫码</a></el-menu-item>
-        <el-menu-item index="3-2"><a href="http://ftrace.baidu.com/ums/cm.html" target="_blank">用户模型</a></el-menu-item>
+        <el-menu-item index="3-1">用户模型扫码</el-menu-item>
+        <a href="http://ftrace.baidu.com/ums/cm.html" target="_blank"><el-menu-item index="3-2">用户模型</el-menu-item></a>
       </el-submenu>
       <el-submenu index="4">
         <template slot="title"><span class="navigation-title">资源流</span></template>
-        <el-menu-item index="4-1"><a href="http://ftrace.baidu.com/zpSearch" target="_blank">正排数据查询</a></el-menu-item>
-        <el-menu-item index="4-2"><a href="http://showx.baidu.com/group/gfeedtrace/report/11060" target="_blank">数据流查询</a></el-menu-item>
+        <a href="http://ftrace.baidu.com/zpSearch" target="_blank"><el-menu-item index="4-1">正排数据查询</el-menu-item></a>
+        <a href="http://showx.baidu.com/group/gfeedtrace/report/11060" target="_blank"><el-menu-item index="4-2">数据流查询</el-menu-item></a>
       </el-submenu>
       <el-submenu index="5">
         <template slot="title"><span class="navigation-title">Debug</span></template>
-        <el-menu-item index="5-1"><a href="http://fdebug.baidu.com/" target="_blank">Debug</a></el-menu-item>
+        <a href="http://fdebug.baidu.com/" target="_blank"><el-menu-item index="5-1">Debug</el-menu-item></a>
       </el-submenu>
       <el-submenu index="6">
         <template slot="title"><span class="navigation-title">实验室</span></template>
@@ -43,8 +38,8 @@
       </el-submenu>
       <el-submenu index="7">
         <template slot="title"><span class="navigation-title">工具箱</span></template>
-        <el-menu-item index="7-1"><a href="http://ftrace.baidu.com/api-trace.html" target="_blank">OpenAPI文档</a></el-menu-item>
-        <el-menu-item index="7-2"><a href="http://wiki.baidu.com/pages/viewpage.action?pageId=304748805" target="_blank">Trace帮助</a></el-menu-item>
+        <a href="http://ftrace.baidu.com/api-trace.html" target="_blank"><el-menu-item index="7-1">OpenAPI文档</el-menu-item></a>
+        <a href="http://wiki.baidu.com/pages/viewpage.action?pageId=304748805" target="_blank"><el-menu-item index="7-2">Trace帮助</el-menu-item></a>
         <el-menu-item index="7-3">截屏定位cuid</el-menu-item>
         <el-menu-item index="7-4">nid批量查询</el-menu-item>
         <el-menu-item index="7-5">用户命中小流量</el-menu-item>
@@ -56,11 +51,6 @@
 <script>
   export default {
     name: 'Navigation',
-    data () {
-      return {
-        activeIndex: '1'
-      }
-    },
     methods: {}
   }
 </script>
@@ -79,11 +69,6 @@
     padding: 0 10%;
     flex-wrap: wrap;
   }
-  .el-menu a{
-    text-decoration: none;
-    color: #fff
-  }
-
   .el-menu-item, .el-submenu {
     flex: 1 !important;
   }
--- src/main.js
+++ src/main.js
@@ -25,6 +25,6 @@ new Vue({
 router.beforeEach((to, from, next) => {
   if (to.meta.title) {
     document.title = to.meta.title
+    next()
   }
-  next()
 })
